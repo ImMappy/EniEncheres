f5703584be3e8102c838319e963edb346c6ffba1
luis <luismariamontero@gmail.com>
2022-07-06 16:03:47 +0200

    ajout encheres et prix dynamique

--- src/main/java/fr/eni/eniencheres/dao/DaoFactory.java
+++ src/main/java/fr/eni/eniencheres/dao/DaoFactory.java
@@ -4,22 +4,28 @@ import fr.eni.eniencheres.dal.ArticleVenduDAL.ArticleVenduDAO;
 import fr.eni.eniencheres.dal.ArticleVenduDAL.ArticleVenduDAOImpl;
 import fr.eni.eniencheres.dal.CategoriesDAL.CategoriesDAO;
 import fr.eni.eniencheres.dal.CategoriesDAL.CategoriesDAOImpl;
+import fr.eni.eniencheres.dal.EncheresDAL.EncheresDAO;
+import fr.eni.eniencheres.dal.EncheresDAL.EncheresDAOImpl;
 import fr.eni.eniencheres.dal.RetraitDAL.RetraitDAO;
 import fr.eni.eniencheres.dal.RetraitDAL.RetraitDAOImpl;
 import fr.eni.eniencheres.dal.UtilisateurDAL.UtilisateursDAO;
 import fr.eni.eniencheres.dal.UtilisateurDAL.UtilisateursDAOImpl;
 
 public class DaoFactory {
-public static UtilisateursDAO getUtilisateursDao(){
-        return new UtilisateursDAOImpl();
-    }
-public static ArticleVenduDAO getArticleVenduDAO() {
-    return new ArticleVenduDAOImpl();}
 
-public static CategoriesDAO getCategoriesDAO(){
-    return new CategoriesDAOImpl();
-}
-public static RetraitDAO getRetraitDAO() {return new RetraitDAOImpl();}
+    public static UtilisateursDAO getUtilisateursDao(){
+            return new UtilisateursDAOImpl();
+        }
+    public static ArticleVenduDAO getArticleVenduDAO() {
+        return new ArticleVenduDAOImpl();}
+    public static EncheresDAO getEncheresDao(){
+        return new EncheresDAOImpl();
+    }
+    public static CategoriesDAO getCategoriesDAO(){
+        return new CategoriesDAOImpl();
+    }
+    public static RetraitDAO getRetraitDAO() {return new RetraitDAOImpl();}
+
 
 
 }
